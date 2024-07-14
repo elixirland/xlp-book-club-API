c7b1ba8e301ac54af21934ffabb9c8ebbc548835
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-07-14 08:41:50 +0200

    Tweak phx.new boilerplate

--- solution/test/phoenix_api_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PhoenixApiWeb.PageControllerTest do
-  use PhoenixApiWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
