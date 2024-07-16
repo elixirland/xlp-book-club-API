02402866d891701c84d6eb4266a04b8bd4bd44eb
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-07-16 12:07:02 +0200

    Add books and pages seeding

--- solution/priv/repo/seeds.exs
+++ solution/priv/repo/seeds.exs
@@ -9,3 +9,86 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+import Ecto.Query, only: [from: 2]
+alias XlPhoenixAPI.Repo
+alias XlPhoenixAPI.Books.{Book, Page}
+
+# Setup
+initial_log_level = Logger.level()
+Logger.configure(level: :info)
+IO.puts("Start database seeding")
+start_time = System.os_time(:millisecond)
+
+# Constants
+n_books = 4000
+n_pages_per_book = 10
+inserted_at = NaiveDateTime.utc_now(:second)
+batch_size = 800
+
+# Insert 4,000 books
+books =
+  for _ <- 1..n_books do
+    %{
+      title: XlFaker.generate_title(),
+      inserted_at: inserted_at,
+      updated_at: inserted_at
+    }
+  end
+
+books
+|> Enum.chunk_every(batch_size)
+|> Enum.each(&Repo.insert_all(Book, &1))
+
+# Batch insert 10 pages per book
+# Some books with have an active page
+book_ids =
+  from(b in Book, select: b.id)
+  |> Repo.all()
+
+pages =
+  book_ids
+  |> Enum.flat_map(fn book_id ->
+    pages =
+      for i <- 1..n_pages_per_book do
+        %{
+          book_id: book_id,
+          content: XlFaker.generate_page(),
+          number: i,
+          status: :inactive,
+          inserted_at: inserted_at,
+          updated_at: inserted_at
+        }
+      end
+
+    List.update_at(
+      pages,
+      :rand.uniform(n_pages_per_book) - 1,
+      &Map.put(&1, :status, Enum.random([:active, :inactive]))
+    )
+  end)
+
+pages
+|> Enum.chunk_every(batch_size)
+|> Enum.each(&Repo.insert_all(Page, &1))
+
+# Teardown
+end_time = System.os_time(:millisecond)
+IO.puts("Finish database seeding")
+IO.puts("Seeded #{n_books} books in #{end_time - start_time}ms")
+Logger.configure(level: initial_log_level)
+
+# Plain
+# [seeding] Inserted 4000 books in 7377ms
+
+# With logger config
+# [seeding] Inserted 4000 books in 3169ms
+
+# With logger config and transaction
+# [seeding] Inserted 4000 books in 4056ms
+
+# With logger config and Repo
+# [seeding] Inserted 4000 books in 986ms
+
+# With logger config and Repo
+# Seeded 40000 books in 18261ms
